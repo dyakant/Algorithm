9b000876ecd77fd9a69ce45f00fea2b88cfc03a2
Dyakov Anton <dyakant@gmail.com>
2021-10-27 00:33:44 +0300

    Complete Cinema exercise

--- src/test/java/les1/Cinema.java
+++ src/test/java/les1/Cinema.java
@@ -6,8 +6,33 @@ import static org.junit.Assert.assertEquals;
 
 public class Cinema {
 
+    // O(n). Проходим по массиву один раз, запоминаем появление занятых мест,
+    // считаем расстояния в зависимости от появления занятых мест
     public static int calcBestDistance(int[] seats) {
-        return 0;
+        int left = -1, // показывает, что появилась первая (левая) единица
+            countZero = 0, // считает количество 0 до и после левых 1
+            dist = 0; // расстояние до свободных мест
+        for (int i = 0; i < seats.length; i++) {
+            if (seats[i] == 0) { // если 0, то просто считаем их
+                countZero++;
+            } else { // 1
+                if (left == -1) { // если это первая 1, то
+                    if (countZero > dist) // проверяем возможные посчитанные 0 до первой единицы
+                        dist = countZero;
+                } else {
+                    countZero = (i - left) / 2; // иначе считаем расстояние между двумя 1
+                    if (countZero > dist) // и сравниваем больше ли оно последнего рассчитанного расстояния
+                        dist = countZero;
+                }
+                    left = i; // запоминаем новое начало 1
+                countZero = 0; // обнуляем счётчик 0
+            }
+            // если массив заканчивается 0,
+            // то нужно проверить посчитанные 0, больше ли они последнего рассчитанного расстояния
+            if (i == (seats.length - 1) && seats[i] == 0 && countZero > dist)
+                dist = countZero;
+        }
+        return dist;
     }
 
     @Test
