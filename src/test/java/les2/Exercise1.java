8d2bdcb640db681728ddfff5581cb2f71ee8ac4f
Dyakov Anton <dyakant@gmail.com>
2021-10-27 23:21:01 +0300

    Update Les2 ex1

--- src/test/java/les2/Exercise1.java
+++ src/test/java/les2/Exercise1.java
@@ -2,6 +2,8 @@ package les2;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+
 import static org.junit.Assert.assertArrayEquals;
 
 // Занятие 2, упражнение 1
@@ -9,35 +11,43 @@ import static org.junit.Assert.assertArrayEquals;
 // слияние этих массивов. Результат – упорядоченный массив;
 public class Exercise1 {
 
-    // Сложность О(n). Последовательный проход по каждому массиву
-    // и копирование в новый
+    // Сложность О(n).
+    // Упорядочить по возрастанию, если массивы упорядочены по убыванию
+    // последовательное копирование элементов из двух массивов в конечный массив
+    // с предварительным сравнением элементов в обоих массивах
     public static int[] mergeArrays(int[] arr1, int[] arr2) {
         int arr1Length = arr1.length;
         int arr2Length = arr2.length;
         int[] res = new int[arr1Length + arr2Length];
 
-        // в зависимости от направления сортировки выбирается необходимый цикл
-        if (arr1[0] < arr1[arr1Length - 1]) {
-            for (int i = 0; i < arr1Length; i++) {
-                res[i] = arr1[i];
+        // Упорядочить по возрастанию, если массив 1 упорядочен по убыванию
+        if (arr1[0] > arr1[arr1Length - 1]) {
+            int tmp;
+            for (int i = 0; i < arr1Length / 2; i++) {
+                tmp = arr1[i];
+                arr1[i] = arr1[arr1Length - 1 - i];
+                arr1[arr1Length - 1 - i] = tmp;
             }
-        } else {
-            int shift = 0;
-            for (int i = arr1Length - 1; i >= 0; i--) {
-                res[shift++] = arr1[i];
+        }
+        // Упорядочить по возрастанию, если массив 2 упорядочен по убыванию
+        if (arr2[0] > arr2[arr2Length - 1]) {
+            int tmp;
+            for (int i = 0; i < arr2Length / 2; i++) {
+                tmp = arr2[i];
+                arr2[i] = arr2[arr2Length - 1 - i];
+                arr2[arr2Length - 1 - i] = tmp;
             }
         }
 
-        // в зависимости от направления сортировки выбирается необходимый цикл
-        int shift = arr1Length; // сдвиг для вставки элементов из второго массива
-        if (arr2[0] < arr2[arr2Length - 1]) {
-            for (int i = 0; i < arr2Length; i++) {
-                res[shift++] = arr2[i];
-            }
-        } else {
-            for (int i = arr2Length - 1; i >= 0; i--) {
-                res[shift++] = arr2[i];
-            }
+        // заполенение конечного массива за один проход
+        int i1 = 0, i2 = 0; // счётчики для каждого массива
+        for (int i = 0; i < res.length; i++) {
+            if (i1 == arr1Length) res[i] = arr2[i2++]; // если 1-ый массив закончился, то копировать только из 2-го
+            else if (i2 == arr2Length) res[i] = arr1[i1++]; // если 2-ой массив закончился, то копировать только из 1-го
+            // взять очередной элемент 1-го массива если он меньше чем очередной элемент 2-го массива, увеличить счётчик 1-го
+            else if (arr1[i1] < arr2[i2]) res[i] = arr1[i1++];
+            // взять очередной элемент 2-го массива если он меньше чем очередной элемент 1-го массива, увеличить счётчик 2-го
+            else res[i] = arr2[i2++];
         }
 
         return res;
@@ -45,22 +55,22 @@ public class Exercise1 {
 
     @Test
     public void testCalculations() {
-        int[] exp = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+        int[] exp = new int[]{1, 2, 3, 4, 5, 5, 6, 7, 8, 9, 10};
 
-        int[] a1 = {1, 2, 3, 4, 5};
-        int[] a2 = {6, 7, 8, 9, 10, 11};
+        int[] a1 = {1, 5, 7, 8, 10};
+        int[] a2 = {2, 3, 4, 5, 6, 9};
         assertArrayEquals(exp, mergeArrays(a1, a2));
 
-        a1 = new int[]{1, 2, 3, 4, 5};
-        a2 = new int[]{11, 10, 9, 8, 7, 6};
+        a1 = new int[]{1, 5, 7, 8, 10};
+        a2 = new int[]{9, 6, 5, 4, 3, 2};
         assertArrayEquals(exp, mergeArrays(a1, a2));
 
-        a1 = new int[]{5, 4, 3, 2, 1};
-        a2 = new int[]{6, 7, 8, 9, 10, 11};
+        a1 = new int[]{10, 8, 7, 5, 1};
+        a2 = new int[]{2, 3, 4, 5, 6, 9};
         assertArrayEquals(exp, mergeArrays(a1, a2));
 
-        a1 = new int[]{5, 4, 3, 2, 1};
-        a2 = new int[]{11, 10, 9, 8, 7, 6};
+        a1 = new int[]{10, 8, 7, 5, 1};
+        a2 = new int[]{9, 6, 5, 4, 3, 2};
         assertArrayEquals(exp, mergeArrays(a1, a2));
     }
 }
